62498616310c73921c7b1266c4a31a53eb536fc1
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-02-08 17:23:44 +0100

    Fix issues with recycler view adapter

--- app/src/main/java/com/spykins/assessment/MainScreen/http/DataFetcher.java
+++ app/src/main/java/com/spykins/assessment/MainScreen/http/DataFetcher.java
@@ -1,20 +1,14 @@
 package com.spykins.assessment.MainScreen.http;
 
-
-
-import java.util.List;
+import com.spykins.assessment.MainScreen.model.ApiResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface DataFetcher {
-    /*@GET("/3/discover/movie?api_key={apiKey}&language=en-US&sort_by=popularity.desc")
-    List<Movie> getMovies();*/
-
-
     @GET("/3/discover/movie")
-    Call<List<FavMovie>> groupList(@Query("api_key") String apiKey,
-                                   @Query("language") String language, @Query("sort_by") String sortBy);
-    //api key = 9a928f01255bbe816cdaa0c5473b8c62
+    Call<ApiResponse> groupList(@Query("api_key") String apiKey,
+                                @Query("language") String language,
+                                @Query("sort_by") String sortBy);
 }
