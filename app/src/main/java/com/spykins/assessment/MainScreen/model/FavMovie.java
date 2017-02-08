46fec2b752d7eb8e33fd15ef921c578a705742b7
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-02-08 19:57:13 +0100

    Implement cardview in the Recycler view

--- app/src/main/java/com/spykins/assessment/MainScreen/model/FavMovie.java
+++ app/src/main/java/com/spykins/assessment/MainScreen/model/FavMovie.java
@@ -1,23 +1,33 @@
 package com.spykins.assessment.MainScreen.model;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class FavMovie {
 
+    @SerializedName("poster_path")
     public String posterPath;
     public Boolean adult;
     public String overview;
+    @SerializedName("release_date")
     public String releaseDate;
-    //public List<Integer> genreIds = null;
+    @SerializedName("genre_ids")
+    public List<Integer> genreIds = null;
     public Integer id;
+    @SerializedName("original_title")
     public String originalTitle;
+    @SerializedName("original_language")
     public String originalLanguage;
     public String title;
+    @SerializedName("backdrop_path")
     public String backdropPath;
     public Float popularity;
+    @SerializedName("vote_count")
     public Integer voteCount;
     public Boolean video;
+    @SerializedName("vote_average")
     public Float voteAverage;
 
     public String toString() {
