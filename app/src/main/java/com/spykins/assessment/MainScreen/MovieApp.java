7b0df39f1468e9adb599b6ceccbdc5c76e2457b4
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-02-08 13:25:47 +0100

    Add annotation plugin for dagger 2

--- app/src/main/java/com/spykins/assessment/MainScreen/MovieApp.java
+++ app/src/main/java/com/spykins/assessment/MainScreen/MovieApp.java
@@ -4,6 +4,8 @@ package com.spykins.assessment.MainScreen;
 import android.app.Application;
 
 import com.spykins.assessment.MainScreen.dagger.AppComponent;
+import com.spykins.assessment.MainScreen.dagger.AppModule;
+import com.spykins.assessment.MainScreen.dagger.DaggerAppComponent;
 
 public class MovieApp extends Application {
 
@@ -12,7 +14,9 @@ public class MovieApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        //appComponent = DaggerAppComponent
+        appComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule())
+                .build();
     }
 
     public AppComponent getAppComponent() {
