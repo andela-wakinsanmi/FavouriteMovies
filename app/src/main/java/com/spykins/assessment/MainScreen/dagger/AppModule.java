1d207c5f92a2f74d2e455af960382edca11564ad
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-02-08 17:01:45 +0100

    Fix issue with runtime error involving java1.7

--- app/src/main/java/com/spykins/assessment/MainScreen/dagger/AppModule.java
+++ app/src/main/java/com/spykins/assessment/MainScreen/dagger/AppModule.java
@@ -7,24 +7,35 @@ import com.spykins.assessment.MainScreen.mainView.view.MainActivity;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class AppModule {
-    String url = "https://api.themoviedb.org";
+    String url = "https://api.themoviedb.org/";
 
     @Provides
     public DataFetcher provideDataFetcher(Retrofit retrofit) {
         return retrofit.create(DataFetcher.class);
     }
 
+    @Provides
+    public OkHttpClient.Builder provideOkHttpLogger() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);
+        return httpClient;
+    }
 
     @Provides
-    public Retrofit provideRetrofit() {
+    public Retrofit provideRetrofit(OkHttpClient.Builder okHttpClient) {
         return new Retrofit.Builder()
                 .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient.build())
                 .build();
     }
 
